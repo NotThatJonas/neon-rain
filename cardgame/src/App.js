b25ea353d2bac005e405c7d0d49a16a0e557f862
collin wheetman <52813167+cwheets@users.noreply.github.com>
2019-10-31 13:05:15 -0700

    created basic cards and place for playing them

--- cardgame/src/App.js
+++ cardgame/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
-
+import Card from "./components/cards"
+import PlayArea from "./components/playArea"
 function App() {
   return (
     <div>
-      
+      <PlayArea/>
+      <div className="row d-flex justify-content-center">
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
     </div>
   );
 }
