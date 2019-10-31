94b1271744bb1d429b40f33085ecd76c6e3c0298
Stimoe <stimoe3@gmail.com>
2019-10-31 13:55:53 -0700

    shuffle deck working

--- cardgame/src/App.js
+++ cardgame/src/App.js
@@ -1,49 +1,16 @@
-// import React, { Component } from "react";
-// // import handleRandom from "./components/ShuffleCards";
-// import cards from "./cards.json"
-
-class Cards extends Component {
-
-  state ={
-    randomCards:[],
-   
-  }
-
-
-componentDidMount() {
-  this.handleRandom(cards);
-}
-
-handleRandom = (cards)=>{ 
-
- 
+import React, { Component } from "react";
+import ShuffleCards from "./components/ShuffleCards";
+import cards from "./cards.json"
 
 
 
-    
-    let randomCardsArray=[]
-    const randomCards = cards.slice(0)
-    for (let i = 0; i < cards.length; i++) {
-    
-       
-    let randomNumber = Math.floor(Math.random() * (i + 1))
-    let temp= randomCards[i]
-    randomCards[i] = randomCards[randomNumber]
-    randomCards[randomNumber] = temp
-    }
-  console.log("randomized ",randomCards);
-  randomCardsArray.push(randomCards)
-  this.setState({
-    randomCardsArray:randomCards,
 
-  });
-  
-  return(
-    console.log(cards)
-  
-  )
-}
-}
+console.log("cards ",cards)
+
+
+
+
+export default Cards
 import React from 'react';
 import Card from "./components/cards"
 import PlayArea from "./components/playArea"
@@ -69,4 +36,4 @@ function App() {
 
 
 
-// export default Cards
+export default App
--- cardgame/src/components/ShuffleCards/index.js
+++ cardgame/src/components/ShuffleCards/index.js
@@ -1,44 +1,36 @@
 import React from "react";
 
-import cards from "../.././cards.json";
+// import cards from "../.././cards.json";
+let randomCards=cards
 
-console.log(cards);
-
-
-
-handleRandom = ()=>{ 
-
-  state ={
-      randomCards:randomCardsArray,
-     
+  class Cards extends Component {
+    state ={
+      randomCardsArray:randomCards,
     }
-
-
-
-    
-    let randomCardsArray=[]
-    const randomCards = cards.slice(0)
-    for (let i = 0; i < cards.length; i++) {
-    
-       
-    let randomNumber = Math.floor(Math.random() * (i + 1))
-    let temp= randomCards[i]
-    randomCards[i] = randomCards[randomNumber]
-    randomCards[randomNumber] = temp
+    componentDidMount() {
+      this.handleRandom();
     }
-  console.log("randomized ",randomCards);
-  randomCardsArray.push(randomCards)
-  this.setState({
-    randomCardsArray:randomCards,
-
-  });
-  
-  return(
-    console.log(cards)
-  
-  )
-}
-
-export default handleRandom;
-
-
+  ShuffleCards = ()=>{ 
+      let randomCardsArray=[]
+      let randomCards = cards.slice(0)
+      for (let i = 0; i < cards.length; i++) {  
+      let randomNumber = Math.floor(Math.random() * (i + 1))
+      let temp= randomCards[i]
+      randomCards[i] = randomCards[randomNumber]
+      randomCards[randomNumber] = temp
+      }
+    console.log("randomized ",randomCards);
+    randomCardsArray.push(randomCards)
+    this.setState({
+      randomCardsArray:randomCards,
+    });
+      console.log(randomCardsArray)
+  }
+  render() {
+    return (
+     <div>
+     </div>
+    )
+  };
+  }
+  export default ShuffleCards;
