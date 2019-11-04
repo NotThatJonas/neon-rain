bafd452f12cda891547bfabfd49f874f07d4b33a
Gabe <gabriel.eyer007@gmail.com>
2019-11-04 13:55:42 -0800

    added bombass styling

--- cardgame/src/pages/login.js
+++ cardgame/src/pages/login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Form from "../components/Form";
-import Login from "../components/login"
+// import Form from "../components/Form";
+import Login from "../components/home"
 
 
 
