e8cea0c8b861142313bb6cf81f35938c68017e1e
Gabe <gabriel.eyer007@gmail.com>
2019-11-06 11:54:21 -0800

    started a game page added styling but havent went power level over 9000 yet

--- cardgame/src/App.js
+++ cardgame/src/App.js
@@ -4,8 +4,8 @@ import Card from "./components/cards"
 import PlayArea from "./components/playArea"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home/home.js";
-import Leaderboards from "./pages/leaderboard"
-
+import Leaderboards from "./pages/leaderboard";
+import Game from "./pages/game"
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
   
         <Route exact path="/leaderboards" component={Leaderboards} />
         <Route exact path="/" component={Home} />      
+        <Route exact path="/game" component={Game} />  
       </div>
         </Router>
 
---
+++ cardgame/src/pages/game/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <div className="landing2">
+          <div className="home-wrap2">
+            <div className="home-inner2"></div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Game;
