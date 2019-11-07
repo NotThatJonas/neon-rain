7b79e5206f57d83ce6e19b01d9304cf58f01e922
Stimoe <stimoe3@gmail.com>
2019-11-07 13:53:28 -0800

    enemy death and new enemy solved

--- cardgame/src/pages/battleArea/index.js
+++ cardgame/src/pages/battleArea/index.js
@@ -2,18 +2,29 @@ import React, { Component } from "react";
 import "./style.css";
 import enemies from "../../enemy.json"
 import DeckBrain from "../../components/deck-managment"
-class BattlePage extends Component {
 
-    state = {
-        userHealth: 100,
-        userArmor: 0,
-        enemyHealth: enemies[0].health,
-        enemyArmor: 0,
-        winCount: 0,
-        playedCards: []
-      };
-    
-    
+
+var newWinCounts;
+var newEnemyObject
+
+class BattlePage extends Component {
+  
+  state = {
+    winCount: 0,
+    userHealth: 100,
+    userArmor: 0,
+    enemies,
+    currentEnemyHealth:0,
+    currentEnemyArmor:0,
+    currentEnemyAttack:0,
+    playedCards: [],
+  };
+  
+
+
+
+
+
     
       cardChoice = id => {
         switch (id) {
@@ -24,105 +35,101 @@ class BattlePage extends Component {
           case 2:
             this.armor(3);
             return;
+
+            // case 3:
+            //   this.multiply(3)
         }
+
       };
     
-    winRound = () => {
-    let newWinCount=(this.state.winCount+1)
-    this.setState=({
-      winCount:newWinCount
-    })
     
-    
-    
+    componentDidMount(){
+let currentEnemy=enemies[this.state.winCount]
+let currentEnemyHealth=currentEnemy.health
+let currentEnemyArmor=currentEnemy.armor
+let currentEnemyAttack=currentEnemy.attack
+      this.setState({
+        currentEnemyHealth:currentEnemyHealth,
+        currentEnemyArmor:currentEnemyArmor,
+        currentEnemyAttack:currentEnemyAttack
+      })
     }
     
-    
-    
+    // multiply
+
+
+
     
       userAttack = damage => {
-        let newArmor;
-        let newHealth;
-        let newDamage;
-        if (this.state.enemyArmor >= damage) {
-          let tempArmor = this.state.enemyArmor;
-          newArmor = tempArmor - damage;
+        let tempArmor=this.state.currentEnemyArmor
+        let tempHealth=this.state.currentEnemyHealth
+        if (this.state.tempArmor >= damage) {
+        tempArmor=tempArmor-damage
+          // newArmor = tempArmor - damage;
           this.setState({
-            enemyArmor: newArmor
+            currentEnemyArmor: tempArmor
           });
-        } else {
-          newDamage = damage - this.state.enemyArmor;
-          let tempHealth = this.state.enemyHealth;
-          newHealth = tempHealth - newDamage;
-          if(newHealth<=0){
-            this.winRound()
-          }
-    
+        } 
+        else {
+          let newDamage = damage - tempArmor;
+          // let tempHealth = tempEnemy.health;
+          tempHealth = tempHealth- newDamage;
+          // tempHealth=newHealth
+          tempArmor=0
           this.setState({
-            enemyHealth: newHealth
+            currentEnemyHealth: tempHealth,
+            currentEnemyArmor:tempArmor
           });
+        if(this.state.currentEnemyHealth<=0){
+          newWinCounts=this.state.winCount+1
+          this.setState({
+            winCount:newWinCounts
+          })
+          // let newEnemy={...this.state.enemies[newWinCounts]}
+          let currentEnemy=enemies[this.state.winCount]
+          let currentEnemyHealth=currentEnemy.health
+          let currentEnemyArmor=currentEnemy.armor
+          let currentEnemyAttack=currentEnemy.attack
+          this.setState({
+            currentEnemyHealth:currentEnemyHealth,
+            currentEnemyArmor:currentEnemyArmor,
+            currentEnemyAttack:currentEnemyAttack
+          })
         }
-      };
-    
+        }
+      }
       armor = armor => {
         let newArmor = this.state.userArmor + armor;
         this.setState({
           userArmor: newArmor
         });
-      };
-    
-    
-    
-     
-    
-    setEnemyValues = enemies => {
-      this.setState({
-        enemyHealth: enemies[this.state.winCount].health,
-        enemyArmor: enemies[this.state.winCount].armor
-      })
-    }
-    
-    
+      };    
     handlePlayedCards = (array)=>{
         let tempPlayed=array
-     
         let self = this
         setTimeout(()=>{
             self.cardChoice(tempPlayed[0].id)
-
-      }, 20)
+      }, 2)
         setTimeout(function(){
             self.cardChoice(tempPlayed[1].id)
-
-      },10)
-    //   this.cardChoice(tempPlayed[1].id)
-
-      
-      
-
+      },1)
     }
-    
-    
       firstEnemyAction = id => {
         let possibleEnemyActions=[]
-        let newEnemyAttack=0
-        let newEnemyArmor=0
+        let newEnemyAttack;
+        let newEnemyArmor;
         for (let k = 0; k < enemies.length; k++) {
            if(id===this.state.winCount){
              newEnemyAttack=enemies[k].attack 
              newEnemyArmor=enemies[k].armor 
             possibleEnemyActions=enemies[k].actions
            }
-          
         }
-    
         let randomAction;
         randomAction = Math.floor(Math.random() * possibleEnemyActions.length);
-    
         switch (randomAction) {
           case 1:
-          
-    
+          //enemy attacks!!
             if (this.state.userArmor >= newEnemyAttack) {
               let newArmor = this.state.userArmor - newEnemyAttack;
               this.setState({
@@ -137,6 +144,8 @@ class BattlePage extends Component {
               });
             }
           case 2:
+
+          //enemy gains armor
             let newArmor = this.state.enemyArmor + newEnemyArmor;
             this.setState({
               enemyArmor: newArmor
@@ -152,6 +161,7 @@ class BattlePage extends Component {
      
         return (
             <div className= "d-flex justify-content-center">
+          
             <DeckBrain readPlayed={this.handlePlayedCards}/>
             </div>
         )}
